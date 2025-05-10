6f6ec1c643016c21c239a5e7152a3f114c2e9290
hadyaalihussein <hadya.232424@univsul.edu.iq>
2025-05-10 22:47:20 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,30 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('services/', views.services, name='services'),
+]
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+def services(request):
+    return render(request, 'main/services.html')
